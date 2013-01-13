db26cda04ba23c519e8af1d56a086cf4257ac7ba
Jozef Reisinger <jozef.reisinger@gmail.com>
2013-01-13 13:16:15 +0100

    Add gen-txt-file.pl

---
+++ code/gen-txt-file.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+# Generate text file of defined size.
+# The size is not very precise, especially for very small files.
+use strict;
+use warnings;
+use autodie;
+
+my $file = shift;
+my $size = shift;
+
+die "Usage: $0 file size_in_bytes\n" if not defined $file or not defined $size;
+
+open my $fh, ">", $file;
+
+while ( -s $file < $size ) {
+    my @words = qw( abcdef ghi jklmno prstuvw xyz );
+    print $fh join( " ", @words ), "\n";
+}
+
+close $fh;

562d9c243a4183d63ee88e3db461397c8d87ea2e
Jozef Reisinger <jozef.reisinger@gmail.com>
2013-10-30 16:14:53 +0100

    add hyperlink script

--- code/moin2md.pl
+++ code/moin2md.pl
@@ -1,11 +1,1 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
-
-while (<>) {
-    s/\[ \/\*[^\*]+\*\/ \]//g;  # remove Slovak translations (moinmoin comments)
-    next if /[^\x00-\x7F]/;     # skip lines with non-ASCII char
-    s/^\s*\*\s*//;                                        # remove leading *
-    s/\[\[([^\|]+)\|([^\]]+)\]\]/<a href=$1>$2<\/a>/g;    # convert links
-    print;
-}
+../bin/moin2md.pl

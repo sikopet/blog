157eb761d4035622bcd63dea2a19960fa81807f4
Jozef Reisinger <jreisinger@users.noreply.github.com>
2015-01-22 16:59:45 +0100

    Update gen-txt-file.pl

--- code/gen-txt-file.pl
+++ code/gen-txt-file.pl
@@ -9,10 +9,11 @@ my $file = shift;
 my $size = shift;
 
 die "Usage: $0 file size_in_bytes\n" if not defined $file or not defined $size;
+die "File '$file' already exists\n" if -e $file;
 
 open my $fh, ">", $file;
 
-while ( -s $file < $size ) {
+while ( -s $file <= $size ) {
     my @words = qw( abcdef ghi jklmno prstuvw xyz );
     print $fh join( " ", @words ), "\n";
 }

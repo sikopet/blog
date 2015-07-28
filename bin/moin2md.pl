81c724db2adb99a7cc6586afd6d1e7295223fab3
Jozef Reisinger <jreisinger@ist.ac.at>
2015-07-28 17:14:31 +0200

    minor change

--- bin/moin2md.pl
+++ bin/moin2md.pl
@@ -3,9 +3,21 @@ use strict;
 use warnings;
 
 while (<>) {
+    next if /^## page was renamed from/;    # skip comments about page renaming
+    next if /[^\x00-\x7F]/;                 # skip lines with non-ASCII char
+
     s/\[ \/\*[^\*]+\*\/ \]//g;  # remove Slovak translations (moinmoin comments)
-    next if /[^\x00-\x7F]/;     # skip lines with non-ASCII char
-    s/^\s*\*\s*//;                                        # remove leading *
-    s/\[\[([^\|]+)\|([^\]]+)\]\]/<a href=$1>$2<\/a>/g;    # convert links
+    s/^\s+\*(.*)$/*$1/;         # remove leading whites space before '*'
+
+    # Convert links: [[http://www.perl.org/|Perl]] => [Perl](http://www.perl.org/)
+    s {
+    \[\[
+    (?<url>[^\|]+)
+    \|
+    (?<link_name>[^\]]+)
+    \]\]
+    }
+    {[$+{url}]($+{link_name})}gx;
+
     print;
 }

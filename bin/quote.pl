be54c96e4bbad03b3eff41710a86ce057843d989
Jozef Reisinger <jreisinger@login1.ista.local>
2015-08-25 09:22:36 +0200

    generate random HTML quote

---
+++ bin/quote.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+# Print a random quote and the image in HTML format.
+# Usage:
+#
+# * * * * *   $HOME/blog/bin/quote.pl > $HOME/public_html/quote.html
+use strict;
+use warnings;
+use LWP::Simple;
+use CGI;
+
+my @quotes = split /\n+/, get
+  "https://raw.githubusercontent.com/jreisinger/blog/master/posts/quotes.txt";
+my $quote = $quotes[ rand @quotes ];
+
+# -- TimToady (http://irclog.perlgeek.de/perl6/2013-07-21)
+# ===>
+# -- <a href="http://irclog.perlgeek.de/perl6/2013-07-21">TimToady</a>
+$quote =~ s#--\s{0,3}([^(]+?)\s+\((http:\/\/[^)]+)\)#-- <a href="$2">$1</a>#;
+
+my $q = CGI->new;
+
+#<<<
+binmode STDOUT, ':utf8';
+print 
+  $q->start_html( -title => "Quote of the Minute", -encoding => "utf-8" ),
+  $q->p($quote),
+  $q->img(
+    {
+        src => "http://imgs.xkcd.com/comics/lisp.jpg",
+        title =>
+          "We lost the documentation on quantum mechanics.  You'\''ll have to decode the regexes yourself.",
+        alt => "Lisp"
+    }
+  ),
+  $q->end_html;
+#>>>

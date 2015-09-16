4c99989a45c5e623f0c6dace55b0a4a3d2a16f08
Jozef Reisinger <jreisinger@ist.ac.at>
2015-09-16 14:05:26 +0200

    Is there at least one symlink, i.e. category, for each post?

--- bin/displayed_posts.pl
+++ bin/displayed_posts.pl
@@ -7,6 +7,7 @@ use 5.010;
 use autodie;
 use File::Find;
 use Text::Autoformat;
+use File::Basename;
 
 my @posts;
 find( sub { push @posts, $File::Find::name if -f && /\.md$/ }, 'posts' );
@@ -19,7 +20,18 @@ while (<$fh>) {
 
 say "Posts that are not displayed in README.md:";
 for my $post ( sort @posts ) {
-    say make_md_link($post) unless grep $post eq $_, @links;
+
+    if ( -l $post ) {
+
+        # convert symlink to basename of its value
+        $post = basename( readlink $post );
+    } else {
+        warn "WARN: $post is not symlinked within any subdirectory\n"
+          unless symlink_exists($post);
+    }
+
+    my $link = make_md_link($post) unless grep $post eq $_, @links;
+    say $link if $link;
 }
 
 # Create link that can be pasted into README under a category
@@ -35,4 +47,18 @@ sub make_md_link {
     $_ = "[$title]($_)";
     s/^/* /;
     return $_;
+}
+
+# Is there at least one symlink, i.e. category, for this post?
+sub symlink_exists {
+    my $post = shift;
+
+    my @symlinks;
+    find( sub { push @symlinks, $File::Find::name if -l && /\.md$/ }, 'posts' );
+
+    for my $link (@symlinks) {
+        return 1 if basename($link) eq basename($post);
+    }
+
+    return;
 }

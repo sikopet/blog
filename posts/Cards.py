ea31efdffe3f88528b7d840601cdfe3d0ec5e331
Jozef Reisinger <jreisinger@users.noreply.github.com>
2017-05-11 07:55:28 +0200

    Create Cards.py

---
+++ posts/Cards.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# Think Python, Ch. 18 Inheritance
+
+class Card:
+    """Represents a poker card"""
+
+    # Integers to names mappings (Class variables)
+    suit_names = [ 'Clubs', 'Diamonds', 'Hearts', 'Spades' ]
+    rank_names = [  None, 'Ace',
+                    '2', '3', '4', '5', '6', '7', '8', '9', '10',
+                    'Jack', 'Queen', 'King' ]
+
+    def __init__( self, suit = 0, rank = 2 ):
+        # (Instance variables)
+        self.suit = suit
+        self.rank = rank
+
+    def __str__( self ):
+        return "%s of %s" % (
+                                # class_variable.[instance_variable]
+                                Card.rank_names[self.rank],
+                                Card.suit_names[self.suit]
+                            )
+
+queen_of_diamonds = Card( 1, 12 )
+print queen_of_diamonds
